cff72dfd188ad542a0e61b881dfabac24d51709c
ALEXANDER <msumba83@gmail.com>
2022-06-02 15:54:43 +0200

    second commit

--- src/components/App.js
+++ src/components/App.js
@@ -3,15 +3,14 @@ import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 
-
 function App() {
-    return (
-      <div>
-        <Header />
-        <Note />
-        <Footer />
-      </div>
-    );
+  return (
+    <div>
+      <Header />
+      <Note />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
